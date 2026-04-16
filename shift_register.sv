7b6923b3a712bb3b2f197fe37199b59fb1c593f5
Laiba Nasir <laibanasir@Mac.lan>
2026-04-16 09:11:34 -0700

    DONE

---
+++ shift_register.sv
@@ -0,0 +1,55 @@
+module shift_register #(
+    // parameter list 
+    // MSB = 1st, LSB =1st 
+    parameter DIRECTION = 1
+)(
+    input logic clk,
+    input logic rst,  // indicates shift register should be reset to all 0s
+    input logic data,
+    input logic wr_en,
+    output logic [31:0] out
+);
+
+// alway blk that triggers on +ve egde 
+// should handle reset and write enable based on DIRECTION 
+always_ff @(posedge clk) begin : blockName
+    // handle reset and write enable 
+    if (rst) begin
+        out <= 32'b0; 
+        
+    end else if (wr_en) begin // u can only shift if write enable is high 
+
+        if (DIRECTION == 1) begin
+            // MSB shift 1st 1100 -> 1110
+            // new bit entrs from MSB side / left side
+            /*
+            - drop right most bit 
+            - shift eerything right by 1 --> by everything i mena, everything after dropping RMS out[31:1]
+            - new bbit filles left most spot --> so pu tdata on left
+            */
+
+            out <= {data, out[31:1]};
+            
+
+            
+        end else begin
+            // LSB shift 1st 0011 -> 0111
+            /*
+                - drop leftmost bit 
+                - shift everything left by 1
+                - new bit fills the rightmost spot
+            */
+
+            
+            out <= {out[30:0], data};
+        end
+        
+    end
+
+
+end
+
+
+// shift logic 
+
+endmodule
